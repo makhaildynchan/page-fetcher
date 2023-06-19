771f2ab6ecda5dd612b2afb10cd236503bd792fc
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-18 21:41:18 -0600

    fetcher,index

---
+++ fetcher.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const fs = require('fs');
+
+const fetch = function(argv) {
+  const url = argv[2];
+  const localPath = argv[3];
+  // http request and wait for the response
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('Error:', error);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      console.log('Invalid response:', response.statusCode);
+      return;
+    }
+
+    fs.writeFile(localPath, body, err => {
+      if (err) {
+        console.error('Error saving file:', err);
+        return;
+      }
+      // success
+      console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
+    });
+  });
+};
+
+fetch(process.argv);
